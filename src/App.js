0db30afefb62dd4682849994881be8d7a70bf10e
Fin-NewWin <nguyenphien17@yahoo.com>
2022-10-24 15:26:11 -0700

    feat: added background and base syntax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [date, setDate] = useState(new Date());
+    return (
+        <div className="App">
+            <div className='intro'>
+                <div className='intro-left'>
+                    <div class="login-form">
+                            <h1>Create Event</h1>
+                            <div class='ex'>
+                                <Calendar
+                                    onChange={setDate}
+                                    value={date}
+                                    selectRange={true}
+                                    />
+                            </div>
+                            {date.length > 0 ? (
+                                <p className='text-center'>
+                                    <span className='bold'>Start:</span>{' '}
+                                    {date[0].toDateString()}
+                                    &nbsp;|&nbsp;
+                                    <span className='bold'>End:</span> {date[1].toDateString()}
+                                </p>
+                            ) : (
+                                    <p className='text-center'>
+                                        <span className='bold'>Default selected date:</span>{' '}
+                                        {date.toDateString()}
+                                    </p>
+                                )}
+                        </div>
+                </div>
+                <div className='intro-right'>
+                    <h2>TODO: pfp</h2>
+                </div>
+            </div>
+            </div>
+    );
 }
 
 export default App;
