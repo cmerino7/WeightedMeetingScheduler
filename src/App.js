f48130926256d0ffba0d24d3e9d87016fc784085
Fin-NewWin <nguyenphien17@yahoo.com>
2022-10-25 22:32:07 -0700

    feat: added toggle switch and range picker, ui change

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import './App.css';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React from 'react';
+
 import { useState } from 'react';
+import { useToggle } from './utils/useToggle';
+
+import 'react-calendar/dist/Calendar.css';
+import './App.css';
 
 function App() {
-    const [date, setDate] = useState(new Date());
+    const [value, onChange] = useState(new Date());
+    const [isOnWeekend, toggleWeekend] = useToggle();
+
+
+
     return (
         <div className="App">
             <div className='intro'>
                 <div className='intro-left'>
                     <div class="login-form">
-                            <h1>Create Event</h1>
-                            <div class='ex'>
-                                <Calendar
-                                    onChange={setDate}
-                                    value={date}
-                                    selectRange={true}
-                                    />
-                            </div>
-                            {date.length > 0 ? (
-                                <p className='text-center'>
-                                    <span className='bold'>Start:</span>{' '}
-                                    {date[0].toDateString()}
-                                    &nbsp;|&nbsp;
-                                    <span className='bold'>End:</span> {date[1].toDateString()}
-                                </p>
-                            ) : (
-                                    <p className='text-center'>
-                                        <span className='bold'>Default selected date:</span>{' '}
-                                        {date.toDateString()}
-                                    </p>
-                                )}
+                        <h1>Create Event</h1>
+                        <button onClick={toggleWeekend}>
+                            Weekend
+                        </button>
+                        <div class='calender-table'>
+                            <Calendar
+                                onChange={onChange}
+                                value={value}
+                                calendarType={"US"}
+                                selectRange={true}
+                                tileDisabled={isOnWeekend ? ({date}) => date.getDay() == 0 || date.getDay() == 6: ({date}) => date.getDay() == null}
+                                minDetail={"month"}
+                                />
                         </div>
+                    </div>
                 </div>
                 <div className='intro-right'>
-                    <h2>TODO: pfp</h2>
+                    <h2>TODO: Other bs</h2>
                 </div>
             </div>
-            </div>
+        </div>
     );
 }
 
