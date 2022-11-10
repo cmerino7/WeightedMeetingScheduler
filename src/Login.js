0f2aa45f1030157c26db06be7567da8d9805bc67
cmerino7 <cmhydra@gmail.com>
2022-11-10 13:23:58 -0800

    added conditional rendering for schedule selector

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ScheduleSide from './ScheduleSide'
+import Message from './Message';
 
+const data = ''
 
 class Login extends React.Component {
     constructor(props) {
@@ -8,8 +10,9 @@ class Login extends React.Component {
       this.state = {value: ''};
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      
     }
-  
+
     handleChange(event) {    
         this.setState({value: event.target.value});  
     }
@@ -19,17 +22,18 @@ class Login extends React.Component {
       //send the name to backend
       event.preventDefault();
     }
-  
     render() {
+      //const data = {};
       return (
+        
         <form onSubmit={this.props.onClickFunc} > 
         {/* <form onSubmit={this.handleSubmit}>    use this to handle events later   */}
             <label>
                 Name:
                 <input type="text" value={this.state.value} onChange={this.handleChange} />    
- 
             </label>
           <input type="submit" value="Submit" />
+          <Message data={data}/>
         </form>
       );
     }
--- src/Message.js
+++ src/Message.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Message(props)
 {
     if (props.isLoggedIn)
-        return <h1 className='logMsg'>Welcome User</h1>;
+        return <h1 className='logMsg'>Welcome {props.data}</h1>;
     else
         return <h1 className='logMsg'>Please log in to select times</h1>;
 }
